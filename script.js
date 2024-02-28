5cd8ec50b1c8bcd4b32e382939adc112d9cdf73e
Javs18 <147437670+Javs18@users.noreply.github.com>
2024-02-28 11:49:11 -0700

    Actividad 8

---
+++ script.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+    .then(reg => console.log('Registro del service worker exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el service worker', err))
+}
